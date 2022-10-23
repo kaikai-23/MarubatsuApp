7072a6bfb8dc6b3bf3ab3d223dd58c5f146360f8
kaikai-23 <gate.sea0203@gmail.com>
2022-10-24 00:01:34 +0900

    戻るボタン実装完了

---
+++ AddQuestioViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddQuestioViewController.swift
+//  MarubatsuApp
+//
+//  Created by 寳門海 on 2022/10/23.
+//
+
+import UIKit
+
+class AddQuestioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backBtnAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
